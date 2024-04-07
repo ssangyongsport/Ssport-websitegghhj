705ef3633e1c13aa24d0a8b203c85dc8e78ad395
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-07 11:50:07 +0800

    Create route.ts

---
+++ pages/api/register /route.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = (await req.json()) as {
+      name: string;
+      email: string;
+      password: string;
+    };
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email: email.toLowerCase(),
+        password,
+      },
+    });
+
+    return NextResponse.json({
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({
+        status: "error",
+        message: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
